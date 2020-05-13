f2860407e5b7ea5236b3867ee59fb669678a79cf
Sam T <48508802+samm4x@users.noreply.github.com>
2020-05-13 15:14:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import datetime
-openLog = open("error_log.txt", "r")
+readLog = open("_log.txt", "rt")
 logFileLine = datetime.datetime.now().strftime("[%a %b  %d %X %Y] [notice] Logs checked by Python script")
-logs = openLog.read()
+errors = []
+lineNum = 0
+errorKeyword = "error".lower()
+for line in readLog:
+  lineNum += 1
+  if line.lower().find(errorKeyword) != -1:
+    foundErrorMSG = "ERROR FOUND! Line: " + str(lineNum)
+    errors.append(foundErrorMSG + "\n" + line)
+readLog.close()
+logMessage = open("_log.txt", "at")
+logMessage.write("\n\n\n" + logFileLine)
+logMessage.close()
+logErrors = open("found_errors.txt", "w")
+for item in errors:
+  logErrors.write("%s\n" % item)
+logErrors.close()
